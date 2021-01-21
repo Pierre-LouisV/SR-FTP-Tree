bbe869f36c08b03d9d8161bf030bc2b164e60e08
virey <pierrelouis.virey.etu@univ-lille.fr>
2021-01-21 17:49:19 +0100

    feat: Ajout d'une fonction ls séparée + choix code silencieux ou qui parle Code d'une fonction ls qui affiche le rep courant. Maintenant on peux choisir si on affiche le contenu du read() ou non.

--- src/main/java/fil/sr1/FTPClient.java
+++ src/main/java/fil/sr1/FTPClient.java
@@ -13,6 +13,7 @@ import java.net.Socket;
  */
 public class FTPClient {
 	
+	private static final boolean TALK = false;
 	private Socket skt;
 	private BufferedReader in;
 	private BufferedWriter out;
@@ -35,21 +36,70 @@ public class FTPClient {
 			out = new BufferedWriter(new OutputStreamWriter(skt.getOutputStream()));
 
 			String reponse = in.readLine();
-			System.out.println(reponse);
+			if(TALK) {System.out.println(reponse);}
 			
 			//ER <SP> <nom d'utilisateur> <CRLF>
 			write("USER anonymous");
-			reponse = in.readLine();
-			System.out.println(reponse);
+			read();
 			
 			//PASSS <SP> <mot de passe> <CRLF>
 			write("PASS guest");
-			reponse = in.readLine();
-			System.out.println(reponse);
+			read();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Execute ls on the current folder of the FTP server.
+	 * @return the raw result of the command.
+	 * @throws IOException 
+	 */
+	public String ls() throws IOException {
+		//Connection passive
+		write("PASV");
+		String reponse = read();
+		
+		//On parse la réponse. On as comme format (IP1,IP2,IP3,IP4,PORT1,PORT2).
+		String[] rep = reponse.split("\\(");
+		String[] repSplited = rep[1].split(",");
+		String ipAdress = repSplited[0] +"."+ repSplited[1] +"."+ repSplited[2] +"."+ repSplited[3]; 
+		if(TALK) {System.out.println(ipAdress);}
+		
+		int port = Integer.parseInt(repSplited[4]) * 256 + Integer.parseInt(repSplited[5].replace(")",""));
+		if(TALK) {System.out.println(port);}
+		
+		//LIST [<SP> <chemin d'accès>] <CRLF> Ici on liste le répertoire courant.
+		write("LIST ");
+		
+		Socket sktData = new Socket(ipAdress, port);
+		
+		read();
+		
+		BufferedReader inData = new BufferedReader(new InputStreamReader(sktData.getInputStream()));
+		
+		String reponse2 = inData.readLine();
+		StringBuilder sb = new StringBuilder();
+		
+		while(reponse2!=null) {
+			sb.append(reponse2).append("\n");
+			reponse2 = inData.readLine();
+		}
+		
+		//Ne change pas grand chose car normalement c'est fermé.
+		//inData.close();
+		//sktData.close();
+		
+		return sb.toString();
+	}
+	
+	/**
+	 * Parse the ls command
+	 * @return the array with the name inside.
+	 */
+	public String[] parseLs(String lsRes) {
+		return null;
 	}
 	
 	public void tree() {
@@ -59,56 +109,26 @@ public class FTPClient {
 			
 			//PWD <CRLF>
 			write("PWD");
-			reponse = in.readLine();
-			System.out.println(reponse);
+			read();
 			
-			//Connection passive
-			write("PASV");
-			reponse = in.readLine();
-			System.out.println(reponse);
-				
-			//On parse la réponse. On as comme format (IP1,IP2,IP3,IP4,PORT1,PORT2).
-			String[] rep = reponse.split("\\(");
+			String lsReponse = ls();
 			
-			String[] repSplited = rep[1].split(",");
+			//System.out.println(lsReponse);
 			
-			String ipAdress = repSplited[0] +"."+ repSplited[1] +"."+ repSplited[2] +"."+ repSplited[3]; 
-			
-			System.out.println(ipAdress);
-			
-			int port = Integer.parseInt(repSplited[4]) * 256 + Integer.parseInt(repSplited[5].replace(")",""));
-			
-			System.out.println(port);
-			
-			//LIST [<SP> <chemin d'accès>] <CRLF>
-			write("LIST /");
-			
-			Socket sktData = new Socket(ipAdress, port);
-			
-			reponse = in.readLine();
-			System.out.println(reponse);
-			
-			BufferedReader inData = new BufferedReader(new InputStreamReader(sktData.getInputStream()));
-			
-			String reponse2 = inData.readLine();
-			StringBuilder sb = new StringBuilder();
-			
-			while(reponse2!=null) {
-				sb.append(reponse2).append("\n");
-				reponse2 = inData.readLine();
-			}
-			
-			String lsReponse = sb.toString();
-			
-			//Ne change pas grand chose.
-			//inData.close();
-			//sktData.close();
-			
-			System.out.println(lsReponse);
+			//On lis le resultat de la commande ls
+			read();
 			
 			write("PWD");
-			reponse = in.readLine();
-			System.out.println(reponse);
+			read();
+			
+			write("CWD cdimage");
+			read();
+			
+			write("PWD");
+			read();
+			
+			lsReponse = ls();
+			System.out.println(lsReponse);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -126,6 +146,22 @@ public class FTPClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Read the string and send it to the server
+	 */
+	public String read() {
+		String reponse = "pb_read";
+		try {
+			reponse = in.readLine();
+			if(TALK)
+				System.out.println(reponse);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return reponse;
 	}
 	
 	
